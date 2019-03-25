25c53a8aedc721608372828cbef8c4301d7bccc9
Minako Nakanishi <n135216e@gmail.com>
2019-03-25 15:26:05 +0900

    バインド属性

--- Vue_book/render/message.js
+++ Vue_book/render/message.js
@@ -4,3 +4,19 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 })
+
+var app2 = new Vue({
+    el: '#app2',
+    data:{
+        message_ja: 'こんにちは',
+        message_en: 'Hello',
+        lang: 'ja'
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data:{
+        message1: 'こんにちは！！！'
+    }
+})
