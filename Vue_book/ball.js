117131c152342b5701901f5a4f6d29376dc806f6
Minako Nakanishi <n135216e@gmail.com>
2019-03-25 11:22:09 +0900

    オブジェクトの階層化

---
+++ Vue_book/ball.js
@@ -0,0 +1,13 @@
+//元の書式
+var ball = {pos:{x:10,y:50}};
+
+// 改行とインデントを使用して見やすく記述
+var ball = {
+    pos:{
+       x: 10,
+       y: 50
+    }
+}
+
+console.log(ball.pos.x);
+console.log(ball.pos.y);
