2ab085a1d7a3a0277cf327397f34b8bec8598e70
Minako Nakanishi <n135216e@gmail.com>
2019-03-25 10:07:44 +0900

    日付の成型確認

---
+++ Vue_book/date.js
@@ -0,0 +1,9 @@
+/** 日付の成型確認 */
+
+var today = new Date();
+
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+console.log(year + "年" + month + "月" + day + "日");
