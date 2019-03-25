6d333b695ec36ea3e2bc587c41f2172513988cc7
Minako Nakanishi <n135216e@gmail.com>
2019-03-25 11:47:08 +0900

    コメントの追加

--- Vue_book/ball.js
+++ Vue_book/ball.js
@@ -2,12 +2,24 @@
 var ball = {pos:{x:10,y:50}};
 
 // 改行とインデントを使用して見やすく記述
+// ボールオブジェクト
 var ball = {
     pos:{
        x: 10,
        y: 50
+    },
+
+    // ボールを動かす関数
+    move: function(a,b){
+        this.pos.x += a;
+        this.pos.y += b;
     }
 }
+
+console.log(ball.pos.x);
+console.log(ball.pos.y);
+
+ball.move(5,3);
 
 console.log(ball.pos.x);
 console.log(ball.pos.y);
