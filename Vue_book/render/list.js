7d87f09acbf5c2dcb4f5c6b178ae5577da5eb590
Minako Nakanishi <n135216e@gmail.com>
2019-03-25 16:30:14 +0900

    リストをテーブルで出力

---
+++ Vue_book/render/list.js
@@ -0,0 +1,11 @@
+
+var list = new Vue({
+    el: '#list',
+    data:{
+        products:[
+            {code: 'A01', name: 'プロダクトA'},
+            {code: 'B01', name: 'プロダクトB'},
+            {code: 'C01', name: 'プロダクトC'}
+        ]
+    }
+})
