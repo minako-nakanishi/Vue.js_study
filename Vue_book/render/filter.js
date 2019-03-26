cb63c8ce335ed3e3556e450d59a8e8281f9aca0b
Minako Nakanishi <n135216e@gmail.com>
2019-03-26 09:33:40 +0900

    filterメソッドの追加

---
+++ Vue_book/render/filter.js
@@ -0,0 +1,8 @@
+/**  フィルター登録 */
+//マスタッシュでテンプレートにバインドしたデータがテキストに出力する前に,何らかの加工を加える事ができる
+// filterの第一引数は「フィルター名」、第二引数は関数オブジェクト.
+// new Vue()を実行するより前に定義する.
+Vue.filter('number_format', function(val){
+    return val.toLocaleString();
+});
+
