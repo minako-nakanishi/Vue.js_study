6f2fd59c5c5fbdb46a2dc0053bae3dbde400a38b
Minako Nakanishi <n135216e@gmail.com>
2019-03-27 10:53:03 +0900

    v-on:input(全角文字入力でもリアルタイムでDOMに反映)

--- Vue_book/form/bind.js
+++ Vue_book/form/bind.js
@@ -5,4 +5,18 @@ var app = new Vue({
         // 初期値は現在の年に指定
         year: (new Date()).getFullYear()
     }
+});
+
+var app2 = new Vue({
+    el: '#app2',
+    data:{
+        year2: (new Date()).getFullYear()
+    },
+
+    // 全角でも、入力した時点でDOMに反映される
+    methods:{
+        yearInputHandler: function($event){
+            this.year2 = $event.target.value;
+        }
+    }
 })
