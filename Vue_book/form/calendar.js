e2494aa2715288d365bb68a3f710f9dbf5d74a90
Minako Nakanishi <n135216e@gmail.com>
2019-03-27 16:00:45 +0900

    今日以前の日付は選択できないように設定

--- Vue_book/form/calendar.js
+++ Vue_book/form/calendar.js
@@ -1,12 +1,14 @@
 var app = new Vue({
     el: '#calendar',
     data:{
-        arrival_date: null
+        arrival_date: null, //到着希望日時
+        min_date: null //選択可能な最短日時
     },
 
     // コンポーネントのインスタンスが生成された直後の挙動
     created: function(){
         this.arrival_date = this.formatDate(new Date());
+        this.min_date = this.arrival_date; //今日以前の日付は選択できないようにした.
     },
     // メソッドの作成
     methods: { //日付をYYYY-MM-DDに整形するメソッド
