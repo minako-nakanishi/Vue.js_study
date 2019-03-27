5517c0829ba99b6e13e887e6a65fdbc491cf9fc4
Minako Nakanishi <n135216e@gmail.com>
2019-03-27 16:46:09 +0900

    レンジと色の同期

--- Vue_book/form/color.js
+++ Vue_book/form/color.js
@@ -6,5 +6,29 @@ var app = new Vue({
         red: 0,
         green: 0,
         blue: 0
-    }
+    },
+
+    computed:{
+        colorElement: function(){
+            return [this.red, this.green, this.blue];
+        }
+    },
+    
+    watch:{
+        colorElement: function(newRGB, oldRGB){
+            var r = ('00' + newRGB[0].toString(16).toUpperCase()).slice(-2);
+            var g = ('00' + newRGB[1].toString(16).toUpperCase()).slice(-2);
+            var b = ('00' + newRGB[2].toString(16).toUpperCase()).slice(-2);
+            this.color = '#' + r + g + b;
+        },
+
+        color:function(newColor, oldColor){
+            this.red = parseInt(newColor.substr(1,2),16);
+            this.green = parseInt(newColor.substr(3,2),16);
+            this.blue = parseInt(newColor.substr(5,2),16);
+        }
+    },
+
+    // カラーパレットの選択変更を監視する
+   
 })
