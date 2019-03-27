c49340fe5bc7d67d3578f6c78ada52fe7daa5a04
Minako Nakanishi <n135216e@gmail.com>
2019-03-27 11:27:42 +0900

    テキストエリアの一方通行バインドと双方向バインド

---
+++ Vue_book/form/textarea.js
@@ -0,0 +1,14 @@
+
+var ippou = new Vue({
+    el: '#ippou',
+    data:{
+        message: 'テキストエリアだよ'
+    }
+});
+
+var souhou = new Vue({
+    el: '#souhou',
+    data:{
+        message1 : 'テキスト双方'
+    }
+})
