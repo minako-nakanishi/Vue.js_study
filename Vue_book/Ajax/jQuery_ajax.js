6d4e127ae25f139f4e79ec56944f2edfc5099c3a
Minako Nakanishi <n135216e@gmail.com>
2019-04-01 14:28:48 +0900

    jQueryのAjax通信で商品一覧を描画する処理を実装

--- Vue_book/Ajax/jQuery_ajax.js
+++ Vue_book/Ajax/jQuery_ajax.js
@@ -11,10 +11,29 @@ function clickHandler(event){ //clickHandler関数を定義
     .done(function(data, textStatus, jqXHR){
         // 通信成功時の処理を記載
         console.log("通信が成功しました");
+        updateScreen(data);
     })
     .fail(function(data, textStatus, jqXHR){
         //通信失敗時の処理を記載
         console.log("通信が失敗しました");
 
-    })
+    });
+}
+
+// 商品一覧の描画
+function updateScreen(products){
+    // 商品リストの子ノードを全て削除する
+    $('#result').empty();
+    var list = '';
+    for(var i=0; i<products.length; i++){
+        list += '<div>';
+        list += "商品ID：" + products[i].id;
+        list += "商品名：" + products[i].name;
+        list += "料金" + products[i].price;
+        list += "画像：" + products[i].image;
+        list += "送料：" + products[i].delv;
+        list += "セール対象：" + products[i].isSale;
+        list += '</div>';
+    }
+    $('#result').append(list);
 }
