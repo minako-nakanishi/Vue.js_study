8799b4f019e4aa5e84b4c361d74d287ceeef3597
Minako Nakanishi <n135216e@gmail.com>
2019-04-01 10:02:57 +0900

    読み込みボタン押下時にHTML要素を取得。クリックイベントを持たせる処理の記載

---
+++ Vue_book/Ajax/ajax.js
@@ -0,0 +1,7 @@
+/** ボタンのHTML要素を取得 */
+var btnload = document.querySelector('#load');
+
+/** 読み込みボタンにクリックイベントを持たせる */
+btnload.addEventListener('click', function(){
+    console.log("ボタンが押されたよ！")
+})
