329d475a48f28ab84a98a96d5a4ba15bfae54f98
Minako Nakanishi <n135216e@gmail.com>
2019-04-05 13:56:15 +0900

    見積自動登録のJSの実装

--- Vue_book/mitumori/movie.js
+++ Vue_book/mitumori/movie.js
@@ -50,6 +50,17 @@ function onPageLoad(event){
     //updateForm();
 }
 
+// 入力内容を変更した時に呼び出されるイベントハンドラ
+function onInputChanged(event){
+    // フォームの表示を更新する.
+    //updateForm();
+}
+
+// --------------------------
+// 関数
+// --------------------------
+
+
 // 日付フォーマット成型用関数
 function formatDate(dt){
     var year = dt.getFullYear(); //現在の年
@@ -65,9 +76,97 @@ function tommorow(){
     return formatDate(today);
 }
 
-function updateForm(){
-    /* 金額を再計算 */
-    
-    /** 表示を更新 */
+// 税抜き金額を税込金額に変更する関数
+function incTax(untaxed){
+    return Math.floor(untaxed * (1 + taxRate));
+}
+
+// 数値を通貨書式「#,###,###」に変換する関数
+function number_format(val){
+    return val.toLocaleString();
+}
+
+// 再計算した基本料金を返す
+function taxedBasePrice(){
+    // 割増料金
+    var addPrice = 0;
+    // フォームコントロールを取得(DVD仕上がり予定日)
+    var delivery_date = app.querySelector('#delivery_date');
+    // 納期までの残り日数を計算
+    var dateDiff = getDateDiff(delivery_date, (new Date()).toLocaleDateString());
+
+    // 割増料金を求める
+    if(21<=dateDiff && dateDiff <30){
+        addPrice = 5000;
+    }else if(14<=dateDiff && dateDiff<21){
+        addPrice = 10000;
+    }else if(7<=dateDiff && dateDiff<14){
+        addPrice = 15000;
+    }else if(3<dateDiff && dateDiff < 7){
+        addPrice = 20000;
+    }else if(dateDiff == 3){
+        addPrice = 40000;
+    }else if(dateDiff == 2){
+        addPrice = 45000;
+    }else if(dateDiff == 1){
+        addPrice = 50000;
+    }else{
+        addPrice = 0;
+    }
+    return incTax(30000 + addPrice);
 
 }
+
+// 再計算したオプション料金(税込)を返す
+function taxedOptPrice(){
+    // オプション料金
+    var optPrice = 0;
+    // フォームコントロールを取得
+    var opt1 = qpp.querySelector('#opt1'); // BGM手配
+    var opt2 = app.querySelector('#opt2'); // 撮影
+    var opt3 = app.querySelector('#opt3'); // DVD盤面印刷
+    var opt4 = qpp.querySelector('#opt4'); // 写真のスキャニング
+
+    // BGM手配
+    if(opt1.checked){optPrice += 5000}
+    // 撮影
+    if(opt2.checked){optPrice += 5000}
+    // DVD盤面印刷
+    if(opt3.checked){optPrice += 5000}
+    // 写真スキャニング
+    if(opt4.value == ''){optPrice = 0}
+    optPrice += opt4.value * 500;
+
+    return incTax(optPrice);
+}
+
+function updateForm(){
+    // フォームコントロールを取得
+    var sum_base = app.querySelector('#sum_base'); //基本料金(税込)
+    var sum_opt = app.querySelector('#sum_opt'); //オプション料金(税込)
+    var sum_total = app.querySelector('#sum_total'); //合計金額(税込)
+
+    /* 金額を再計算 */
+    var basePrice = taxedBasePrice(); //基本料金(税込)
+    var optPrice = taxedOptPrice(); //オプション料金(税込)
+    var totalPrice = basePrice + optPrice; //合計金額(税込)
+
+    /** 表示を更新 */
+    sum_base.value = number_format(basePrice); //基本料金(税込)
+    sum_opt.value = number_format(optPrice); //オプション料金(税込)
+    sum_total.value = number_format(totalPrice); //合計金額(税込)
+}
+
+// 日付の差を求める関数
+function getDateDiff(dateString1, dateString2){
+    // 日付を表す文字列から日付オブジェクトを生成
+    var date1 = new Date(dateString1);
+    var date2 = new Date(dateString2);
+
+    // 2つの日付の差分(ミリ秒)を計算
+    var msdiff = date1.getTime() - date2.getTime();
+
+    // 求めた差分をミリ秒に変換
+    return Math.ceil(msdiff/(1000 * 60 * 60 * 24));
+}
+
