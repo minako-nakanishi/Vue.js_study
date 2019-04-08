a31a77916b65b1108884974fd2c6a2a7a8793b4c
Minako Nakanishi <n135216e@gmail.com>
2019-04-08 16:11:39 +0900

    Vueのコンポーネントの基本構造を記載

--- module/1/show-hello.js
+++ module/1/show-hello.js
@@ -1,2 +1,37 @@
 
-Vue.component('show-hello',{template: '<p>Hello Vue!</p>'})
+/** Vue.component()の基本構造 */
+
+// コンポーネント
+Vue.component('show-hello',{
+    // テンプレート部分
+    template: '<p>{{message}}</p>',
+    // dataオプション
+    data: function(){
+        return{
+        message: 'Hello, Vue!!!!!!'
+       }
+    },
+    // メソッド
+    methods:{
+    
+    },
+    // 算出プロパティ
+    computed: {
+
+    },
+
+    // ウォッチャ
+    watch:{
+
+    },
+
+    // フィルタ
+    filters:{
+
+    },
+
+    // ライフサイクルハック
+    created: function(){
+
+    }
+});
