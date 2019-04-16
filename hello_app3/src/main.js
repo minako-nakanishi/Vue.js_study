cf4462a180fdea7557edf0f9d0ad285290bc48d8
Minako Nakanishi <n135216e@gmail.com>
2019-04-16 17:47:55 +0900

    hello_app3プロジェクトのmain.jsにコメントを記載

--- hello_app3/src/main.js
+++ hello_app3/src/main.js
@@ -1,8 +1,12 @@
+/** プロジェクトをWebアプリケーションとして起動した際に
+最初に実行されるスクリプト */
+
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // Vue起動時に表示されるチップス情報をON/OFFする機能.
 
+// Vueオブジェクトの作成
 new Vue({
   render: h => h(App),
 }).$mount('#app')
